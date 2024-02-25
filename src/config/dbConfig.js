9fb0fb940ca26457176efb4440b4b6ff2ca4c1bf
Paulo Cardoso <64991125+PauloEgidioCardoso@users.noreply.github.com>
2024-02-25 20:50:06 -0300

    added create user end login with jwt token

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,18 @@
-const senha = 'PnI676EYwbyw7uUe';
-
 const mongoose = require('mongoose');
 
-const dbConfig = 'mongodb+srv://usuario:PnI676EYwbyw7uUe@apicluster.ffrzk8o.mongodb.net/annotations?retryWrites=true&w=majority';
+//change environments to get .env
+const user = 'usuario';
+const password = 'PnI676EYwbyw7uUe';
+
+const dbConfig = `mongodb+srv://${user}:${password}@apicluster.ffrzk8o.mongodb.net/annotations?retryWrites=true&w=majority`;
 
 const connection = mongoose.connect(dbConfig, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+}).catch((err) => {
+    console.log('Erro ao conectar ao banco de dados:', err);
 });
 
 module.exports = connection;
